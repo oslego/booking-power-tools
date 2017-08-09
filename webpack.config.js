1478e41370966b08fdd40f6ee52ff59c1d9dd734
Razvan Caliman <razvan.caliman+dev@gmail.com>
2017-08-09 10:30:23 +0200

    add chrome dev webpack plugin to manage manifest; simplify build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,54 @@
-const commonConfig = require('./webpack.common');
-const webpackMerge = require('webpack-merge');
+const path = require("path");
+const webpack = require('webpack');
+const ChromeDevPlugin = require("chrome-dev-webpack-plugin");
+const UglifyJSWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env) => {
   console.log(env);
-  const envConfig = require(`./webpack.${env.env}.js`);
-  return webpackMerge(commonConfig, envConfig);
+
+  const isDev = (env.env === "dev") ? true : false;
+
+  const javascript = {
+    test: /\.(js)$/,
+    use: [{
+      loader: 'babel-loader',
+      options: { presets: ['es2015'] }
+    }]
+  };
+
+  const chromeExtensionPlugin = new ChromeDevPlugin({
+    //The source manifest file you want to use for your extension
+    entry: './src/manifest.json',
+    package: './package.json',
+    buildId: false,
+  })
+
+  const globalVariablesPlugin = new webpack.DefinePlugin({
+    DEV_MODE: isDev,
+  })
+
+  const uglifyJSPlugin = new UglifyJSWebpackPlugin({
+    sourceMap: false
+  });
+
+  const plugins = isDev
+    ? [ globalVariablesPlugin, chromeExtensionPlugin ]
+    : [ globalVariablesPlugin, chromeExtensionPlugin, uglifyJSPlugin ];
+
+  const config = {
+    entry: {
+      contentscript: './src/contentscript.js',
+      background: './src/background.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'chrome-extension'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [ javascript ]
+    },
+    plugins: plugins
+  };
+
+  return config;
 }
